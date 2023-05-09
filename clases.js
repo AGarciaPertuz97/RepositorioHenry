095492408f93102aa756a51837934ef1c35efbfc
rockam97 <andresg_2297@hotmail.com>
2023-05-09 15:59:40 -0500

    Actualizo terminación módulo 7

---
+++ clases.js
@@ -0,0 +1,47 @@
+// FUNCIÓN CONSTRUCTORA
+
+// function Auto(puertas, color, marca, año, ruedas) {
+//     this.puerta = puertas;
+//     this.color = color;
+//     this.marca = marca;
+//     this.año = año;
+//     this.ruedas = ruedas;
+
+//     this.informacion = function (){ 
+//         console.log("Este es un " + this.marca + " de color " + this.color);
+//     }
+// }
+
+// var miPrimerAuto = new Auto (2, 'rojo', 'Ferrari', 2023, 4);
+
+// miPrimerAuto.informacion();
+
+// // console.log(miPrimerAuto);
+// // console.log(miPrimerAuto.marca);
+
+
+// EXPRESIÓN DE CLASE
+
+// class Auto {
+//     constructor(puertas, color, marca, año, ruedas){
+//         this.puerta = puertas;
+//         this.color = color;
+//         this.marca = marca;
+//         this.año = año;
+//         this.ruedas = ruedas;
+//     }
+
+//     informacion() {
+//         console.log("Este es un " + this.marca + " de color " + this.color);
+//     }
+// }
+
+// var miSegundoAuto = new Auto(4, "Blanco", "Fiat", 2015, 4);
+
+
+// miSegundoAuto.informacion();
+// // console.log(miSegundoAuto);
+// // console.log(miSegundoAuto.marca);
+
+
+
